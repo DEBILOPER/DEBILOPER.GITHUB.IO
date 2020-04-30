d1d06c46b3a0c842b2ef30f22f31fa1ad9ad32e2
maxangelo987 <perinmaks@outlook.ph>
2020-04-30 15:33:33 +0800

    Updates

---
+++ precache-manifest.4e115a8eee098afaad58c56c3fb58643.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d0e91b233b8b3bfad2ca",
+    "url": "/static/css/main.a63ae376.chunk.css"
+  },
+  {
+    "revision": "d0e91b233b8b3bfad2ca",
+    "url": "/static/js/main.d0e91b23.chunk.js"
+  },
+  {
+    "revision": "1466b53d9190348e94ed",
+    "url": "/static/js/1.1466b53d.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "818000c4069044ed03799815152c59e6",
+    "url": "/index.html"
+  }
+];
